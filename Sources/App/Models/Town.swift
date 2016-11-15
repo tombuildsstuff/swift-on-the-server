d9e74d9d4e591d388f6516b6624786e7ae08bad6
tombuildsstuff <git@ibuildstuff.co.uk>
2016-11-15 21:41:14 +0200

    Adding a Town model - used to return a list of Towns as JSON

---
+++ Sources/App/Models/Town.swift
@@ -0,0 +1,13 @@
+import JSON
+
+struct Town {
+    let name: String
+    let country: String
+    
+    func toJson() throws -> JSON {
+        return try JSON(node: [
+            "name": self.name,
+            "country": self.country
+        ])
+    }
+}
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,3 +1,4 @@
+import HTTP
 import Vapor
 
 let drop = Droplet()
@@ -10,6 +11,23 @@ drop.get { req in
 
 drop.get("/build-stuff") { req in
     return "Hello Build Stuff!"
+}
+
+drop.get("/towns") { req in
+    let towns = [
+        Town(name: "Winchester", country: "England"),
+        Town(name: "Vilnius", country: "Lithuania"),
+        Town(name: "Kiev", country: "Ukraine"),
+        Town(name: "Sienna", country: "Italy"),
+        Town(name: "Taormina", country: "Sicily"),
+        Town(name: "Bilbao", country: "Spain"),
+        Town(name: "Lucca", country: "Italy"),
+        Town(name: "Cupertino", country: "America")
+    ]
+    let json = try towns.map({ (town: Town) -> JSON in
+        return try town.toJson()
+    })
+    return try Response(status: .ok, json: JSON(json))
 }
 
 drop.resource("posts", PostController())
